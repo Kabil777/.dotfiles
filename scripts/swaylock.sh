074f387debd46301f25fc3099e5805d279807eae
Kabil-ARCH <kabilmuthu777@gmail.com>
2025-01-21 13:56:00 +0530

    Adjusted configs

--- scripts/swaylock.sh
+++ scripts/swaylock.sh
@@ -1,2 +1,2 @@
-swayidle -w timeout 60 'swaylock -C /home/kabil/.config/sway_lock/config -f' timeout 120 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
-
+swayidle -w timeout 180 'swaylock -C /home/kabil/.config/sway_lock/config -f' timeout 240 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+ 
